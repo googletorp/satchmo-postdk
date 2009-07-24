6f5a016c79717581a7b1c6a2b0846b38b2063071
Jakob Torp Svendsen <jakob@revealit.dk>
2009-07-24 11:51:19 +0200

    Debugging syntax/typo errors

--- helper.py
+++ helper.py
@@ -4,7 +4,6 @@ cost of sending the the merchandise bought by the users. The prices
 used will be in Danish Crowens (DKK), the end result however can be
 overridden to any currency through the admin interface.
 """
-
 from exceptions import ImportError, KeyError
 
 try:
@@ -35,16 +34,16 @@ class Parcel(object):
         except KeyError:
             self.width = None
         try:
-            self.weigth = kwargs['weight']
+            self.weight = kwargs['weight']
         except KeyError:
             self.weight = None
         self.products = []
         try:
-            self.volume = self.height * self.length * self.width
-        except TypeError:
+            self.volume = kwargs['volume']
+        except KeyError:
             try:
-                self.volume = kwargs['volume']
-            except KeyError
+                self.volume = self.height * self.length * self.width
+            except TypeError:
                 self.volume = None
 
     def volume_fee(self):
@@ -52,7 +51,7 @@ class Parcel(object):
         Method to calculate if volume fee should be added to parcel.
         """
         count = 0
-        for length in (self.height * self.width * self.length):
+        for length in (self.height, self.width, self.length):
             if length > 150:
                 return True
             if length > 50:
@@ -86,24 +85,24 @@ def pack_parcels(cartset, settings):
     for ci in cartset:
         product = ci.product
         default = False
-        if not (product.width and product.height and product.lenght):
+        if not (product.width and product.height and product.length):
             default = True
-        if not (product.width_units.lower() and products.length_units.lower() \
+        if not (product.width_units.lower() and product.length_units.lower() \
                 and product.height_units.lower()) != 'cm':
             default = True
         if default:
-            ci.product.width = settings['POSTDK_DEFAULT_WIDTH']
-            # ci.product.width_units = settings['POSTDK_DEFAULT_SIZE_UNITS']
-            ci.product.length = settings['POSTDK_DEFAULT_LENGTH']
-            # ci.product.length_units = settings['POSTDK_DEFAULT_SIZE_UNITS']
-            ci.product.height = settings['POSTDK_DEFAULT_HEIGHT']
-            # ci.product.height_units = settings['POSTDK_DEFAULT_SIZE_UNITS']
+            ci.product.width = settings['POSTDK_DEFAULT_WIDTH'].value
+            # ci.product.width_units = settings['POSTDK_DEFAULT_SIZE_UNITS'].value
+            ci.product.length = settings['POSTDK_DEFAULT_LENGTH'].value
+            # ci.product.length_units = settings['POSTDK_DEFAULT_SIZE_UNITS'].value
+            ci.product.height = settings['POSTDK_DEFAULT_HEIGHT'].value
+            # ci.product.height_units = settings['POSTDK_DEFAULT_SIZE_UNITS'].value
         if not product.weight or product.weight_units.lower() != 'kg':
-            ci.product.weight = settings['POSTDK_DEFAULT_HEIGHT']
-            # ci.product.weight_units = settings['POSTDK_DEFAULT_SIZE_UNITS']
+            ci.product.weight = settings['POSTDK_DEFAULT_HEIGHT'].value
+            # ci.product.weight_units = settings['POSTDK_DEFAULT_SIZE_UNITS'].value
 
     # One parcel per cart item.
-    if settings['POSTDK_PACKING'] == 'CART'
+    if settings['POSTDK_PACKING'].value == 'CART':
         result = []
         for ci in cartset:
             if ci.product.is_shippable:
@@ -112,7 +111,7 @@ def pack_parcels(cartset, settings):
                         height=ci.product.height,
                         length=ci.product.length,
                         width=ci.product.width,
-                        weight=ci.product.width
+                        weight=ci.product.weight
                     )
                     parcel.products.append(ci.product)
                     result.append(parcel)
@@ -120,31 +119,33 @@ def pack_parcels(cartset, settings):
     # Pack based on weight, up to the maximum volume set by admins. This is
     # not optimised to try to fit as many products into a single pasel as
     # possible.
-    elif settings['POSTDK_PACKING'] == 'SINGLE':
+    elif settings['POSTDK_PACKING'].value == 'SINGLE':
         result = []
-        max_volume = settings['POSTDK_PERCENT_VOLUME'] * 2500
+        max_volume = settings['POSTDK_PERCENT_VOLUME'].value * 2500
         parcel = Parcel(weight=0, volume=0)
         for ci in cartset:
-            for n in range(ci.quantity):
-                if ci.product.weight > 20
-                or get_volume(ci.product) > max_volume:
-                    new_parcel = Parcel(
-                        weight=ci.product.weight,
-                        length=ci.product.length,
-                        width=ci.product.width
-                        weight=ci.product.width
-                    )
-                    new_parcel.products.append(ci.product)
-                    result.append(new_parcel)
-                else:
-                    if parcel.weight + ci.product.weight > 20 \
-                    or parcel.volume + get_volume(ci.product) > max_volume:
-                        result.append(parcel)
-                        parcel = Parcel(weight=0, volume=0)
-                    parcel.products.append(ci.product)
-                    parcel.volume +=  get_volume(ci.product)
-                    parcel.weight += ci.product.weight
-        result.append(parcel)
+            if ci.product.is_shippable:
+                for n in range(ci.quantity):
+                    if ci.product.weight > 20 \
+                        or get_volume(ci.product) > max_volume:
+                        new_parcel = Parcel(
+                            weight=ci.product.weight,
+                            length=ci.product.length,
+                            width=ci.product.width,
+                            weight=ci.product.width
+                        )
+                        new_parcel.products.append(ci.product)
+                        result.append(new_parcel)
+                    else:
+                        if parcel.weight + ci.product.weight > 20 or \
+                            parcel.volume + get_volume(ci.product) > max_volume:
+                            result.append(parcel)
+                            parcel = Parcel(weight=0, volume=0)
+                        parcel.products.append(ci.product)
+                        parcel.volume +=  get_volume(ci.product)
+                        parcel.weight += ci.product.weight
+        if parcel.weight > 0:
+            result.append(parcel)
 
     return result
 
